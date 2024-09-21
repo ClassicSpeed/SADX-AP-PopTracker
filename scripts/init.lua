2e15a676abeb91f857a46a03a3f7c195fa151b5d
Classic <ClassicSpeed@users.noreply.github.com>
2024-09-21 09:15:49 -0300

    Initial commit for starting project

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,13 +1,11 @@
-Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/modifiers.json")
 
 Tracker:AddMaps("maps/maps.json")
-
 ScriptHost:LoadScript("scripts/logic.lua")
 
+Tracker:AddLocations("locations/locations.json")
+Tracker:AddLocations("locations/HotelArea.json")
+Tracker:AddLocations("locations/EmeraldCoast.json")
+
+Tracker:AddItems("items/items.json")
 Tracker:AddLayouts("layouts/items.json")
-
-ScriptHost:LoadScript("scripts/loadlocations.lua")
-ScriptHost:LoadScript("scripts/autotracking.lua")
-
-Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/standard.json")
